e673a66df6174f86698c58e3d2c59d7ed3ac43ab
drskavoj <vojtech.drska@gmail.com>
2020-12-10 15:50:22 +0100

    [FIX] Checking if any tournament IDs exist

--- src/main/java/Application/Database/Service/PlayerService.java
+++ src/main/java/Application/Database/Service/PlayerService.java
@@ -1,18 +1,16 @@
 package Application.Database.Service;
 
 import Application.Database.DTO.PlayerCreateDTO;
-import Application.Database.DTO.PlayerDTO;
 import Application.Database.Enity.Club;
 import Application.Database.Enity.Player;
 import Application.Database.Enity.Tournament;
 import Application.Exceptions.InstanceNotFoundException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 
 @Service
@@ -34,12 +32,16 @@ public class PlayerService extends BaseService implements PlayerServiceInterface
     }
 
     @Override
+    @Transactional
     public Player create (PlayerCreateDTO playerCreateDTO) throws InstanceNotFoundException {
         Club club = getIfExists(playerCreateDTO.getClubId(), clubRepository);
         Set<Integer> tournamentIDs = playerCreateDTO.getTournamentIDs();
-        Set<Tournament> tournaments = new HashSet<>();
-        for(int id : tournamentIDs){
-            tournaments.add(getIfExists(id, tournamentRepository));
+        Set<Tournament> tournaments = null;
+        if(tournamentIDs != null) {
+            tournaments = new HashSet<>();
+            for (int id : tournamentIDs) {
+                tournaments.add(getIfExists(id, tournamentRepository));
+            }
         }
         Player player = new Player(playerCreateDTO.getName(), playerCreateDTO.getBirthDate(),
                 playerCreateDTO.getBigPoints(), club, tournaments);
@@ -47,12 +49,17 @@ public class PlayerService extends BaseService implements PlayerServiceInterface
     }
 
     @Override
+    @Transactional
     public Player update(Integer id, PlayerCreateDTO playerCreateDTO) throws InstanceNotFoundException{
         Player player = getIfExists(id, playerRepository);
         Club club = getIfExists(playerCreateDTO.getClubId(), clubRepository);
-        Set<Tournament> tournaments = new HashSet<>();
-        for(int tournamentID : playerCreateDTO.getTournamentIDs()){
-            tournaments.add(getIfExists(tournamentID, tournamentRepository));
+        Set<Integer> tournamentIDs = playerCreateDTO.getTournamentIDs();
+        Set<Tournament> tournaments = null;
+        if(tournamentIDs != null) {
+            tournaments = new HashSet<>();
+            for (int id2 : tournamentIDs) {
+                tournaments.add(getIfExists(id2, tournamentRepository));
+            }
         }
         player.setBigPoints(playerCreateDTO.getBigPoints());
         player.setBirthDate(playerCreateDTO.getBirthDate());
@@ -63,6 +70,7 @@ public class PlayerService extends BaseService implements PlayerServiceInterface
     }
 
     @Override
+    @Transactional
     public void remove(Integer id) throws InstanceNotFoundException {
         Player player = getIfExists(id, playerRepository);
         playerRepository.delete(player);
