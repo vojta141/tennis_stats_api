92c08fd862c58195b07530f922c96e5c561d6844
drskavoj <vojtech.drska@gmail.com>
2020-12-10 15:54:06 +0100

    [EDIT] HAL enabled

--- src/main/java/Application/Application.java
+++ src/main/java/Application/Application.java
@@ -2,10 +2,11 @@ package Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 import java.sql.Connection;
 @SpringBootApplication
-
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class Application {
 
     private static Connection connection;
